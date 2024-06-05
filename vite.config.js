378e3092f4848a55fed2781eb4dc8c544106a7fd
Natthapong Kanna <natthapong_ka65@live.rmutl.ac.th>
2024-06-06 06:20:36 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://backendjobify.vercel.app/api",
+        target: "https://backendjobify.onrender.com/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
